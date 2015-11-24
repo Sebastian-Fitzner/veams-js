1add4e3427be439d153ee3966c19e504c6d887b0
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-11-24 08:04:56 +0100

    updated module class, added preRender

--- js/modules/_global/module.js
+++ js/modules/_global/module.js
@@ -14,25 +14,58 @@ class AppModule {
 		this.initialize();
 	}
 
+	// GETTER AND SETTER
+
+	/**
+	 * Return options
+	 */
 	get _options() {
 		return this.options;
 	}
 
+	/**
+	 * Save options by merging default options with passed options
+	 */
 	set _options(options) {
 		this.options = Helpers.defaults(options || {}, this.options);
 	}
 
+	// STANDARD METHODS
+
+	/**
+	 * Initialize your module class,
+	 * save some references,
+	 * optionally scaffold some templates and
+	 * bind your events
+	 */
 	initialize() {
+		this.preRender();
 		this.bindEvents();
 	}
 
+	/**
+	 * Bind global and local events
+	 */
 	bindEvents() {
 	}
 
+	/**
+	 * Pre-Render templates
+	 * which can be used to render content into it
+	 */
+	preRender() {
+	}
+
+	/**
+	 * Render your module
+	 */
 	render() {
 	}
 }
 
+/**
+ * Add mixin functionality to extend module class
+ */
 AppModule.classMixin = Helpers.classMixin;
 
 export default AppModule;
