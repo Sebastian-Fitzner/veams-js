b2e4a56f73779da29f9b3dc34a1e7df9d3f94ad9
Andy Gutsche <andy.gutsche@aperto.de>
2016-03-21 16:45:32 +0100

    removed custom module events (only keep global events)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+### v3.0.9
+- removed custom module events (only keep global events)
+
 ### v3.0.8
 - added INSERTPOINT to events.js
--- js/utils/events.js
+++ js/utils/events.js
@@ -4,24 +4,16 @@
  * @author: Sebastian Fitzner
  */
 
+/**
+ * Events Global
+ */
+
 const EVENTS = {
-	btnClose: 'button:close',
-	btnOpen: 'button:open',
 	DOMchanged: 'DOMchanged',
 	DOMredirect: 'dom:redirect',
-	equalRender: 'equal:render',
-	formComplete: 'form:complete',
-	formReset: 'form:reset',
 	mediachange: 'mediachange',
-	paginationClose: 'pagination:closed',
-	paginationRendered: 'pagination:rendered',
 	resize: 'resize',
-	scroll: 'scroll',
-	selectChanged: 'select:changed',
-	selectRendered: 'select:rendered',
-	toggleContent: 'toggle:toggleContent',
-	uiOverlayPopulate: 'overlay:populate',
-	uiOverlayClose: 'uiOverlay:populate'
+	scroll: 'scroll'
 };
 
 // @INSERTPOINT :: @ref: js-events
